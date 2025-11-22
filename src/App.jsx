6da99811783077fb48eb072a6aff0b78940142b3
Aditya <adityasuri11ss@gmail.com>
2025-11-22 19:19:45 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import NewsLetter from './components/NewsLetter'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <NewsLetter/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
