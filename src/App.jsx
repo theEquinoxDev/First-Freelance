a95dfc1c5189a8ba2d97c52df4c166f372685472
Aditya <adityasuri11ss@gmail.com>
2025-11-22 21:37:35 +0530

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import NewsLetter from './components/NewsLetter'
-import Hero from './components/Hero'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './components/About'
 
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    <NewsLetter/>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
