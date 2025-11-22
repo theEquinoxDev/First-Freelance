c5662ad8516bd180121cb86f86a1e242229e13b0
Aditya <adityasuri11ss@gmail.com>
2025-11-22 21:37:00 +0530

    changed layout for navigation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import NewsLetter from '../components/NewsLetter'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <NewsLetter />
+    </>
+  )
+}
